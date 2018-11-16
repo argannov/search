e80030d042c99e82d94dbd872bf96fe7fbf98409
argannov <argannov@yandex.ru>
2018-11-16 11:39:20 +0300

    структура проекта

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" media="screen" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/css/bootstrap.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <title></title>
+    <link rel="stylesheet" href="css/style.css">
+    <script src=""></script>
+</head>
+
+<body>
+
+
+
+</body>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</html>
